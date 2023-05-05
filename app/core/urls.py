0e21b3b2a54cb1fad2cbd31a9c85edbddd418264
Juan Arriagada <jarrcan@gmail.com>
2023-05-05 11:43:19 -0400

    🚧 ci inicial

--- app/core/urls.py
+++ app/core/urls.py
@@ -5,7 +5,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("sso/", include("sso.urls")),
-    path("polls/", include("polls.urls")),
-    path("cbvpolls/", include("cbvpolls.urls")),
 ]
